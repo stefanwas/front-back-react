6bd04ca58638cfd38dbc9cceac9457daa955da71
Wojciech_Krzysztofik <wojciech_krzysztofik@epam.com>
2019-03-14 16:49:55 +0100

    fixing back

--- back/src/main/java/com/stefan/Application.java
+++ back/src/main/java/com/stefan/Application.java
@@ -1,11 +1,14 @@
 package com.stefan;
 
 
-import org.omg.CORBA.Environment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 import javax.annotation.Resource;
 
-@SpringBootApplication(scanBasePackages = {'com.stefan'})
+@SpringBootApplication(scanBasePackages = {"com.stefan"})
 public class Application {
     @Resource
     private Environment environment;
